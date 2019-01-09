2b22bbec04c51919b38ca4444d3467854fca4510
💫响当当😜 <chunmeng.jiang@live.com>
2019-01-09 09:50:25 +0800

    init commit

---
+++ ex-producer.py
@@ -0,0 +1,74 @@
+# /usr/bin/env python3
+
+import sys
+import time
+import redis
+import configparser
+
+r: redis.Redis = None
+POST_SEQ_KEY = 'phone_number_post'
+CONSUME_SEQ_KEQ = 'phone_number_seq'
+cf: configparser.ConfigParser = None
+
+
+PHONE_INTERVAL = [130, 131, 132, 133, 134, 135, 136, 137, 138, 139, 145, 146, 147, 148, 149, 150, 151, 152, 153, 155,
+                  156, 157, 158, 159, 165, 166, 170, 171, 172, 173, 174, 175, 176, 177, 178, 180, 181, 182, 183, 184,
+                  185, 186, 187, 188, 189, 191, 198, 199]
+
+
+def __init():
+    """系统初始化"""
+    global r, cf
+    cf = configparser.ConfigParser()
+    cf.read('env.conf')
+    r = redis.Redis(host=cf.get("redis", 'DB_HOST'), port=cf.get("redis", "DB_PORT"), db=cf.getint("redis", "DB_INDEX"),
+                    password=cf.get("redis", 'DB_PASSWORD'), decode_responses=True)
+
+
+def main():
+    __init()
+    global PHONE_INTERVAL
+    prefix, cur_number = next_sequence()
+    post_seq = [i for i in PHONE_INTERVAL if i <= prefix]
+    for i in post_seq:
+        for next_number in range(cur_number, 100000000):
+            while r.llen(CONSUME_SEQ_KEQ) >= 100000:
+                sys.stdout.write('当前队列剩余 {0} 条 , 等待执行...\r'.format(r.llen(CONSUME_SEQ_KEQ)))
+                sys.stdout.flush()
+                time.sleep(5)
+
+            number_str = str(i) + str(next_number).zfill(8)
+
+            pipe = r.pipeline()
+            if next_number == 0:
+                pipe.hset(POST_SEQ_KEY, i, 0)
+            else:
+                pipe.hincrby(POST_SEQ_KEY, i, 1)
+
+            pipe.rpush(CONSUME_SEQ_KEQ, number_str)
+            pipe.execute()
+
+            sys.stdout.write('当前投递序列 {0} \r'.format(number_str))
+            sys.stdout.flush()
+
+        else:
+            cur_number = 0
+
+
+def next_sequence():
+    """获取下一个待投递的号码序列"""
+    for prefix in PHONE_INTERVAL:
+        fs = r.hget(POST_SEQ_KEY, prefix)
+        if fs is None:
+            return prefix, 0
+        else:
+            fs = int(fs)
+            if fs < 99999999:
+                return prefix, fs
+
+    print('所有号码，均已经投递完成 .....')
+    exit(0)
+
+
+main()
+
---
+++ ex-spider.py
@@ -0,0 +1,69 @@
+#!/usr/bin/env python
+
+import sys
+import redis
+import signal
+import requests
+import configparser
+
+r: redis.Redis = None
+CONSUME_SEQ_KEY = 'phone_number_seq'
+GET_PHONES_KEY = 'validate_phones'
+cf: configparser.ConfigParser = None
+process_number: str = None
+
+REQUEST_TOKEN = 'bearer: eyJhbGciOiJIUzUxMiIsInR5cCI6IkpXVCJ9.eyJhdWQiOiJkc19tZXNzZW5nZXIiLCJleHAiOjE1NDkxNzk2Mjg'\
+                'sImlhdCI6MTU0NjU4NzYyOCwiaXNzIjoiZHNfbWVzc2VuZ2VyIiwianRpIjoiNjE5ZmIwNzktYWEwYi00OWZmLTgxMGMtMDJ'\
+                'kM2RjMmJhNjRlIiwibmJmIjoxNTQ2NTg3NjI3LCJyZXNvdXJjZV90eXBlIjoidXNlciIsInN1YiI6IjYyOTgiLCJ0eXAiOiJ'\
+                'hY2Nlc3MifQ.uqjMfeV7NLJs54k-skCRSFkxt_MOMlmXX-odmow2XxWl6zenVbVB3Zgi0jn-SoUbrJSlVsW9BYGBwKSkAaCoeg'
+
+
+def __init():
+    """系统初始化"""
+    global r, cf
+    cf = configparser.ConfigParser()
+    cf.read("env.conf")
+    r = redis.Redis(host=cf.get("redis", 'DB_HOST'), port=cf.get("redis", "DB_PORT"), db=cf.getint("redis", "DB_INDEX"),
+                    password=cf.get("redis", 'DB_PASSWORD'), decode_responses=True)
+
+
+def main():
+    __init()
+    while True:
+        _, phone = r.blpop(CONSUME_SEQ_KEY, 20)
+        phone = str(phone)
+        request(phone)
+
+
+def request(phone: str):
+    global process_number
+    process_number = phone
+    try:
+        res = requests.get('http://api.echat188.com:9000/users?mobile=%s' % phone,
+                           headers={'User-Agent': 'okhttp/3.0.1', 'authorization': REQUEST_TOKEN, 'Platform': 'android',
+                                    'AppVersion': '1.0.6'}, timeout=2)
+    except requests.exceptions.BaseHTTPError:
+            r.lpush(CONSUME_SEQ_KEY, phone)
+            print('phone %s net work error' % phone)
+    else:
+        if res.status_code != 200:
+            r.lpush(CONSUME_SEQ_KEY, phone)
+        else:
+            if res.json()['data']['users']:
+                r.rpush(GET_PHONES_KEY, phone)
+            else:
+                print('phone %s is not found' % phone)
+        res.close()
+
+
+def halt(signum, frame):
+    if process_number:
+        r.lpush(CONSUME_SEQ_KEY, '%s' % process_number)
+        print("sync phone_number [%s]" % process_number)
+    print("Bye Bye ....")
+    sys.exit()
+
+
+signal.signal(signal.SIGINT, halt)
+signal.signal(signal.SIGTERM, halt)
+main()
